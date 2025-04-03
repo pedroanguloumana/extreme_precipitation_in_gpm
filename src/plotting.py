bd4e2f9da42f317cb84c755b2340b3661822881f
Pedro <pedro@Big-Papa.local>
2025-04-03 08:06:32 -0700

    thesis version

--- src/plotting.py
+++ src/plotting.py
@@ -6,5 +6,5 @@ def array_midpoints(x):
 
 def save_figure(fig, filename):
     path_to_save = f'/Users/pedro/extreme_precipitation_in_gpm/figures/{filename}.pdf'
-    fig.savefig(path_to_save, format='pdf')
+    fig.savefig(path_to_save, format='pdf', bbox_inches='tight')
 
